020f6d023e07217cbc9e1d74980a4929bd5d49e9
Alex Uldahl <alex@uldahl.dk>
2026-08-12 09:19:51 +0200

    map over products and child component

--- src/listProducts.tsx
+++ src/listProducts.tsx
@@ -1,3 +1,22 @@
+import {useState} from "react";
+
 export function ListProducts() {
-    return <div>Welcome</div>;
+
+    const [products, setProducts] = useState([
+        {id: 1, title: "my product 1"},
+        {id: 2, title: "my other product"}])
+
+    return <div>
+
+        {
+            products.map(p => {
+                return <MyChildComponent key={p.id} />
+            })
+        }
+
+    </div>;
+}
+
+function MyChildComponent() {
+    return <div>hello world</div>
 }
