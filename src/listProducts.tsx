5533af603d948e9b59173a65b872169316f7214a
Alex Uldahl <alex@uldahl.dk>
2026-08-12 09:25:42 +0200

    react props

--- src/listProducts.tsx
+++ src/listProducts.tsx
@@ -1,22 +1,26 @@
 import {useState} from "react";
-
+interface Product {
+    id: number;
+    title: string;
+}
 export function ListProducts() {
 
-    const [products, setProducts] = useState([
+    const [products, setProducts] = useState<Product[]>([
         {id: 1, title: "my product 1"},
         {id: 2, title: "my other product"}])
 
     return <div>
-
         {
             products.map(p => {
-                return <MyChildComponent key={p.id} />
+                return <MyChildComponent key={p.id} product={p}/>
             })
         }
-
     </div>;
 }
 
-function MyChildComponent() {
-    return <div>hello world</div>
+interface MyChildComponentProps {
+    product?: Product
+}
+function MyChildComponent({product}: MyChildComponentProps) {
+    return <div>the product is: {product?.title}</div>
 }
